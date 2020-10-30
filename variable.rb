d93d99d18c0a3ea96fac83164cbc4b0754a060c1
nanaama527 <72888369+nanaama527@users.noreply.github.com>
2020-10-29 21:50:09 -0400

    Done.

--- variable.rb
+++ variable.rb
@@ -1,11 +1,7 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 
- def greeting
-    greeting = "Hello World"
- end
- 
-greeting
 
+   greeting = "Hello World"
 
 
